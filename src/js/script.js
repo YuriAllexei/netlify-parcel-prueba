1f76a3cd2caa5eba34ccb2339c7cc5418aaa3e3a
YuriAllexei <yurialexei@ciencias.unam.mx>
2021-09-08 22:58:23 -0500

    first commit yeeeaaaahhh boooooy

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+import cloneDeep from "lodash-es";
+
+console.log("bambambgam");
+
+const obj = {
+  cart: [
+    {
+      prod: "bla",
+      quant: 2,
+    },
+    {
+      prod: "bli",
+      quant: 6,
+    },
+  ],
+};
+
+const copia = cloneDeep(obj);
+
+console.log(copia);
+
+console.log("dana hermosa");
